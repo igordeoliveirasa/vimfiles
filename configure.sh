1932604e770a9f5d3c1f2fcacc16102af4a46799
Igor de Oliveira Sa <igordeoliveirasa@gmail.com>
2013-09-01 08:57:34 -0300

    ok

--- configure.sh
+++ configure.sh
@@ -1,4 +1,4 @@
 mkdir -p ~/.vim/autoload;
 curl -Sso ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim;
-call copy_vimrc.sh
+sh copy_vimrc.sh;
 git submodule update --init;
